ae16aa4757ba15e2a27ae2f224f7b27fd27846f8
Tymkoski <trymek7707@gmail.com>
2025-06-11 20:37:51 +0000

    sprite

--- game.py
+++ game.py
@@ -1,13 +1,17 @@
 import pygame
 from screen import Screen
-def game():
-    pygame.init()
-    running = True
-    screen = Screen()
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.surface.blit(screen.game_map, (0, 0))
-        pygame.display.flip()
-        screen.clock.tick(screen.fps)
+from player import Player
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = Screen()
+        self.player = Player(100, 100) 
+        self.running = True 
+    def game():
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.screen.surface.blit(self.screen.game_map, (0, 0))
+            pygame.display.flip()
+            self.screen.clock.tick(self.screen.fps)
