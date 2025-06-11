d32714dd5bd879180b79272eed667a72339f7778
Tymkoski <trymek7707@gmail.com>
2025-06-11 20:25:05 +0000

    player.py character.py

---
+++ character.py
@@ -0,0 +1,57 @@
+import pygame
+
+class Character:
+    def __init__(self , x ,y = 0, max_health = 100, speed = 5, size = 50 , damage = 10, range = 100, sprite_path = None):
+       
+        self.x = x
+        self.y = y
+
+       
+        self.max_health = max_health
+        self.health = max_health
+        self.speed = speed
+        self. size = size
+        
+        self.alive = True
+        self.direction_x = 0
+        self.direction_y = 0
+        self.sprite = None
+        self.sprite_rect = None
+
+        self.damage = damage
+        self.range = range
+        try:
+            self.sprite = pygame.image.load(sprite_path)
+            self.sprite = pygame.transform.scale(self.sprite, (self.size, self.size))
+            self.sprite_rect = self.sprite.get_rect()
+        except pygame.error as e:
+            print(f"Error loading sprite: {e}")
+            self.sprite = None
+            self.sprite_rect = None
+            
+    def draw(self ,screen):
+        screen.surface.blit(self.sprite, (self.x, self.y))
+
+    def move(self):
+        self.x += self.direction_x * self.speed
+        self.y += self.direction_y * self.speed
+
+    def die(self):
+        self.alive = False
+        self.health = 0
+        
+    def take_damage(self, amount):
+        self.health -= amount
+        if self.health <= 0:
+            self.die()
+    def heal(self, amount):
+        if self.alive:
+            self.health += amount
+            if self.health > self.max_health:
+                self.health = self.max_health
+    
+
+    def collide(self, other):
+        if self.sprite_rect and other.sprite_rect:
+            return self.sprite_rect.colliderect(other.sprite_rect)
+        return False
--- player.py
+++ player.py
@@ -1,5 +1,24 @@
 import pygame 
-
-class Player:
-    def __init__(self):
-        
+from character import Character
+class Player(Character):
+    def __init__(self , x ,y):
+        super().__init__(x, y , max_health=100, speed=5, size=25 , sprite_path="./assets/player.png")
+        self.experience = 0
+        self.level = 1
+    def input(self):
+        keys = pygame.key.get_pressed()
+        self.driection_x = 0
+        self.direction_y = 0
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.direction_y = -1
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.direction_y = 1
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.direction_x = -1
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.direction_x = 1
+    def update(self):
+        self.input()
+        self.move()
+    
+    
